1a80dadce4167a969332f2923b51e0c7ab21affd
arifananta0 <ananta.arif30@gmail.com>
2025-01-13 20:37:44 +0700

    Add local file index.jsx

--- src/components/Elements/Stepper/index.jsx
+++ src/components/Elements/Stepper/index.jsx
@@ -1,49 +1,74 @@
-import * as React from 'react';
+
+import { useContext, useState } from 'react';
 import { useTheme } from '@mui/material/styles';
+import { ThemeContext } from '../../../context/ThemeContext';
 import MobileStepper from '@mui/material/MobileStepper';
 import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-export default function DotsMobileStepper() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
+const Stepper = (props) => {
+    const { desc } = props;
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = useState(0);
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
 
-  return (
-    <MobileStepper
-      variant="dots"
-      steps={6}
-      position="static"
-      activeStep={activeStep}
-      sx={{ maxWidth: 100, flexGrow: 1 }}
-      nextButton={
-        <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
-          Next
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </Button>
-      }
-      backButton={
-        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
-          Back
-        </Button>
-      }
-    />
-  );
-}
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const dataNum = desc.length;
+
+    const { theme: themeMode } = useContext(ThemeContext);
+    
+    return (
+        <>
+        <div>{desc[activeStep]}</div>
+        <MobileStepper
+            variant="dots"
+            steps={dataNum}
+            position="static"
+            activeStep={activeStep}
+            sx={{ 
+                maxWidth: "100%", 
+                flexGrow: 1,
+                "& .MuiMobileStepper-dot": {
+                    backgroundColor: "darkgray",
+                },
+                "& .MuiMobileStepper-dotActive": {
+                    backgroundColor: themeMode.color,
+                } 
+            }}
+
+
+            nextButton={
+                <Button size="small" onClick={handleNext} disabled={activeStep === dataNum - 1}>
+                    Next
+                    {theme.direction === 'rtl' ? (
+                        <KeyboardArrowLeft />
+                    ) : (
+                        <KeyboardArrowRight />
+                    )}
+                </Button>
+            }
+            backButton={
+                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                    {theme.direction === 'rtl' ? (
+                        <KeyboardArrowRight />
+                    ) : (
+                        <KeyboardArrowLeft />
+                    )}
+                    Back
+                </Button>
+            }
+        />
+        </>
+    );
+};
+
+export default Stepper;
+>>>>>>> e5d2acc (Add local file index.jsx)
